c86ee25cab62515664f4a7a98deba87e3d5f7c7b
andreasHovaldt <andreas.hovaldt@gmail.com>
2024-10-22 12:40:21 +0200

    Switched to using the morty cup for feature matching

--- sift_matcher.py
+++ sift_matcher.py
@@ -1,34 +1,44 @@
+import os
 import cv2
-import matplotlib.pyplot as plt
 import time
-
+import matplotlib.pyplot as plt
 
 ######################
-USE_CV2_SIFT = False #
+USE_CV2_SIFT = False
 ######################
 
-box = cv2.imread('resources/box.png', 0)
-box_in_scene = cv2.imread('resources/box_in_scene.png', 0)
 
+# Loading images
+CWD = os.path.dirname(__file__)
+
+cup1 = cv2.imread(os.path.join(CWD, 'resources/cup1.JPG'))
+cup1 = cv2.resize(cup1, (0,0), fx=0.3, fy=0.3) # Resize to make processing faster
+cup1_gray = cv2.cvtColor(cup1, cv2.COLOR_BGR2GRAY)
+
+cup2 = cv2.imread(os.path.join(CWD, 'resources/cup2.JPG'))
+cup2 = cv2.resize(cup2, (0,0), fx=0.3, fy=0.3)
+cup2_gray = cv2.cvtColor(cup2, cv2.COLOR_BGR2GRAY)
+
+
+# Run the SiFT algorithm on the images
 print("Sift is running...")
 if not USE_CV2_SIFT:
     from sift import sift
     
     t = time.time()
-    kpts1, dscpts1 = sift(box)
-    kpts2, dscpts2 = sift(box_in_scene)
+    kpts1, dscpts1 = sift(cup1_gray)
+    kpts2, dscpts2 = sift(cup2_gray)
     print(f"SiFT execution time: {time.time() - t}")
 
-if USE_CV2_SIFT:
+elif USE_CV2_SIFT:
     t = time.time()
     cv2_sift: cv2.SIFT = cv2.SIFT_create()
-    kpts1, dscpts1 = cv2_sift.detectAndCompute(box, None)
-    kpts2, dscpts2 = cv2_sift.detectAndCompute(box_in_scene, None)
+    kpts1, dscpts1 = cv2_sift.detectAndCompute(cup1_gray, None)
+    kpts2, dscpts2 = cv2_sift.detectAndCompute(cup2_gray, None)
     print(f"SiFT execution time: {time.time() - t}")
 
 
-
-# Init matcher
+# Match the found keypoints and descriptors pairs
 print("Matching keypoints and descriptors...")
 matcher = cv2.BFMatcher()
 matches = matcher.knnMatch(dscpts1, dscpts2, k=2)
@@ -38,8 +48,10 @@ good = []
 for m,n in matches:
     if m.distance < 0.75*n.distance:
         good.append([m])
-        
-matched_img = cv2.drawMatchesKnn(box,kpts1,box_in_scene,kpts2,good,None,flags=2)
+
+
+# Draw matches and show figure
+matched_img = cv2.drawMatchesKnn(cup1,kpts1,cup2,kpts2,good,None,flags=2)
 
 plt.imshow(cv2.cvtColor(matched_img, cv2.COLOR_BGR2RGB))
 plt.show()
